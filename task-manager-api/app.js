7bccd4f9f2cb5c50ef551bf45b48434a96c760d7
suhdanny <suhdong21@gmail.com>
2022-08-04 18:43:56 -0400

    Implement all routes within routes and controllers folder

--- task-manager-api/app.js
+++ task-manager-api/app.js
@@ -1,1 +1,10 @@
-console.log('Task Manager App')
+const express = require('express');
+const tasks = require('./routes/tasks');
+const app = express();
+
+app.use(express.json());
+app.use('/api/v1/tasks', tasks);
+
+app.listen(3000, () => {
+	console.log('Server listening on port 3000...');
+});
