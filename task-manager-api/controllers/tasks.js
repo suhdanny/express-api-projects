79ddea579a7bb2826ac5ff4d482d2698b69f1874
suhdanny <suhdong21@gmail.com>
2022-08-04 23:05:54 -0400

    Update dependencies

--- task-manager-api/controllers/tasks.js
+++ task-manager-api/controllers/tasks.js
@@ -2,18 +2,26 @@ const getAllTasks = (req, res) => {
 	res.send('All items');
 };
 
-const createTask = (req, res) => {};
+const createTask = (req, res) => {
+	res.send('Create task');
+};
 
-const getTask = (req, res) => {};
+const getSingleTask = (req, res) => {
+	res.send('Get single task');
+};
 
-const updateTask = (req, res) => {};
+const updateTask = (req, res) => {
+	res.send('Update task');
+};
 
-const deleteTask = (req, res) => {};
+const deleteTask = (req, res) => {
+	res.send('Delete task');
+};
 
 module.exports = {
 	getAllTasks,
 	createTask,
-	getTask,
+	getSingleTask,
 	updateTask,
 	deleteTask,
 };
---
+++ task-manager-api/db/connect.js
@@ -0,0 +1,1 @@
+const connectionString = 'mongodb+srv://suhdanny:<password>@expressprojects.wbblysl.mongodb.net/?retryWrites=true&w=majority';
