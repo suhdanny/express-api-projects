96c726b6d453750ffe6a5ad4e792366d7cafb06f
suhdanny <suhdong21@gmail.com>
2022-08-06 12:26:02 -0400

    Implement CRUD operations

--- task-manager-api/controllers/tasks.js
+++ task-manager-api/controllers/tasks.js
@@ -1,21 +1,64 @@
-const getAllTasks = (req, res) => {
-	res.send('All items');
+const { findOneAndDelete } = require('../models/Task');
+const Task = require('../models/Task');
+
+const getAllTasks = async (req, res) => {
+	try {
+		const tasks = await Task.find({});
+		res.status(200).json(tasks);
+	} catch (error) {
+		res.status(500).json({ msg: error });
+	}
 };
 
-const createTask = (req, res) => {
-	res.send('Create task');
+const createTask = async (req, res) => {
+	try {
+		const task = await Task.create(req.body); // create a task and store it in DB
+		res.status(200).json(task);
+	} catch (error) {
+		res.status(500).json({ msg: error });
+	}
 };
 
-const getSingleTask = (req, res) => {
-	res.send('Get single task');
+const getSingleTask = async (req, res) => {
+	try {
+		const { id } = req.params;
+		const task = await Task.findOne({ _id: id });
+		if (!task) {
+			return res.status(404).json({ msg: `No task with the given ID ${id} was found.` });
+		}
+		res.status(200).json(task);
+	} catch (error) {
+		res.status(500).json({ msg: error });
+	}
 };
 
-const updateTask = (req, res) => {
-	res.send('Update task');
+const updateTask = async (req, res) => {
+	try {
+		const { id } = req.params;
+		const task = await Task.findOneAndUpdate({ _id: id }, req.body, {
+			new: true,
+			runValidators: true,
+		});
+		if (!task) {
+			return res.status(404).json({ msg: `No task with the given ID ${id} was found.` });
+		}
+		res.status(200).json(task);
+	} catch (error) {
+		res.status(500).json({ msg: error });
+	}
 };
 
-const deleteTask = (req, res) => {
-	res.send('Delete task');
+const deleteTask = async (req, res) => {
+	try {
+		const { id } = req.params;
+		const task = await Task.findOneAndDelete({ _id: id });
+		if (!task) {
+			return res.status(404).json({ msg: `No task with the given ID ${id} was found.` });
+		}
+		res.status(200).json(task);
+	} catch (error) {
+		res.status(500).json({ msg: error });
+	}
 };
 
 module.exports = {
--- task-manager-api/models/Task.js
+++ task-manager-api/models/Task.js
@@ -1,8 +1,16 @@
 const mongoose = require('mongoose');
 
 const taskSchema = new mongoose.Schema({
-	name: String,
-	completed: Boolean,
+	name: {
+		type: String,
+		required: [true, 'You must provide the task name.'],
+		trim: true,
+		maxLength: [20, 'The name should be at most 20 characters.'],
+	},
+	completed: {
+		type: Boolean,
+		default: false,
+	},
 });
 
 const Task = mongoose.model('Task', taskSchema);
