3c02fa1f9980a2e9267c6016b6e675c72c8522a9
suhdanny <suhdong21@gmail.com>
2022-08-12 00:01:21 -0400

    Add project boilerplate with model schema

--- store-api/app.js
+++ store-api/app.js
@@ -1,1 +1,34 @@
-console.log('04 Store API')
+require('dotenv').config();
+require('express-async-errors');
+
+const connectDB = require('./db/connect');
+
+const productsRouter = require('./routes/products');
+const notFoundMiddleware = require('./middleware/not-found');
+const errorMiddleware = require('./middleware/error-handler');
+
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+	res.send("<h1>Store API</h1><a href='/api/v1/products'>Products API Page</a>");
+});
+
+app.use('/api/v1/products', productsRouter);
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
+
+const start = async () => {
+	try {
+		await connectDB(process.env.MONGO_URI);
+		app.listen(5000, () => {
+			console.log('Server running on port 5000.');
+		});
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+start();
